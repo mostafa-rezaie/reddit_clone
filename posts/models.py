87aef0ad61ed74d60c70c846847cbc3acb9940ac
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-06-18 00:21:23 +0430

    initial commit

--- posts/models.py
+++ posts/models.py
@@ -1,17 +1,25 @@
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     url = models.URLField()
-    poster = models.ForeignKey(User,on_delete=models.CASCADE)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering  = ['-created']
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'<{self.title}>'
+
 
 class Vote(models.Model):
-    voter = models.ForeignKey(User,on_delete=models.CASCADE)
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.voter}'s vote for {self.post}"
--- posts/serializer.py
+++ posts/serializer.py
@@ -1,10 +1,23 @@
 from dataclasses import field
 from pyexpat import model
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Vote
+
 
 class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
+    votes = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields = ['id','title','url','poster','created']
-        
+        fields = ['id', 'title', 'url', 'poster', 'created', 'poster_id', 'votes']
+
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['id']
